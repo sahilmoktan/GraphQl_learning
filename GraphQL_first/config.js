80cc3c70dc0380b2b9b8f36238a256bd4c217e5e
sahilmoktan <moktanshail9@gmail.com>
2024-03-13 01:14:19 +0545

    successfully connected with mogodb Atlas

---
+++ GraphQL_first/config.js
@@ -0,0 +1,1 @@
+export const MONGO_URI ="mongodb+srv://admin:admin@cluster0.lnovzcc.mongodb.net/graphql?retryWrites=true&w=majority&appName=Cluster0"
--- GraphQL_first/server.js
+++ GraphQL_first/server.js
@@ -1,9 +1,26 @@
 import { ApolloServer, gql } from "apollo-server";
 import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
 import typeDefs from "./SchemaGQl.js";
+import mongoose from "mongoose";
+import { MONGO_URI } from "./config.js";
+
+mongoose.connect(MONGO_URI,{
+  useNewUrlParser:true,
+  useUnifiedTopology:true
+})
+
+mongoose.connection.on("connected",()=>{
+  console.log("Connected to Mongodb")
+})
+
+mongoose.connection.on("error",()=>{
+  console.log("error connecting", err)
+})
+
+
+import "./models/Quotes.js"
+import "./models/User.js"
 import resolvers from "./resolvers.js";
-
-
 
 
 
